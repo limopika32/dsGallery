b8fa05bc4a9ea7df92676621b6bb70351b3e8562
limopika <c1104121@planet.kanazawa-it.ac.jp>
2024-10-02 02:07:54 +0900

    add to music page : listview

--- dsGallery/dsGallery/view/ApplicationCollection.cs
+++ dsGallery/dsGallery/view/ApplicationCollection.cs
@@ -12,12 +12,14 @@ namespace dsGallery.view
         public string Title { get; set; }
         public string Detail { get; set; }
         public string ImageLocation { get; set; }
+        public string Path { get; set; }
 
-        public ApplicationCollection(string Title, string Detail, string ImageLocation)
+        public ApplicationCollection(string Title, string Detail, string ImageLocation, string Path)
         {
             this.Title = Title;
             this.Detail = Detail;
             this.ImageLocation = ImageLocation;
+            this.Path = Path;
         }
     }
 
@@ -25,11 +27,11 @@ namespace dsGallery.view
     {
         public ApplicationCollections()
         {
-            Add(new ApplicationCollection("Sample1", "loremipsum1", "/Resources/Illust/costco.png"));
-            Add(new ApplicationCollection("Sample2", "loremipsum2", "/Resources/Illust/costco.png"));
-            Add(new ApplicationCollection("Sample3", "loremipsum3", "/Resources/Illust/costco.png"));
-            Add(new ApplicationCollection("Sample4", "loremipsum4", "/Resources/Illust/costco.png"));
-            Add(new ApplicationCollection("Sample5", "loremipsum5", "/Resources/Illust/costco.png"));
+            Add(new ApplicationCollection("Sample1", "loremipsum1", "/Resources/Illust/costco.png", ""));
+            Add(new ApplicationCollection("Sample2", "loremipsum2", "/Resources/Illust/costco.png", ""));
+            Add(new ApplicationCollection("Sample3", "loremipsum3", "/Resources/Illust/costco.png", ""));
+            Add(new ApplicationCollection("Sample4", "loremipsum4", "/Resources/Illust/costco.png", ""));
+            Add(new ApplicationCollection("Sample5", "loremipsum5", "/Resources/Illust/costco.png", ""));
         }
     }
 }
--- dsGallery/dsGallery/view/music.xaml.cs
+++ dsGallery/dsGallery/view/music.xaml.cs
@@ -14,6 +14,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Media.Core;
 using Windows.Media.Playback;
+using Microsoft.UI.Xaml.Media.Animation;
+using Windows.Foundation.Metadata;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
@@ -24,13 +26,61 @@ namespace dsGallery.view
     /// </summary>
     public sealed partial class music : Page
     {
-
+        MusicCollection _storeditem;
         MediaPlayer mediaPlayer = new MediaPlayer();
 
         public music()
         {
             this.InitializeComponent();
-            
+            // Ensure that the MainPage is only created once, and cached during navigation.
+            this.NavigationCacheMode = NavigationCacheMode.Enabled;
+
+        }
+
+        private void collection_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Get the collection item corresponding to the clicked item.
+            if (collection.ContainerFromItem(e.ClickedItem) is ListViewItem container)
+            {
+                // Stash the clicked item for use later. We'll need it when we connect back from the detailpage.
+                _storeditem = container.Content as MusicCollection;
+
+                // Prepare the connected animation.
+                // Notice that the stored item is passed in, as well as the name of the connected element.
+                // The animation will actually start on the Detailed info page.
+                collection.PrepareConnectedAnimation("ForwardConnectedAnimation", _storeditem, "mainContent");
+            }
+
+            // Navigate to the DetailedInfoPage.
+            // Note that we suppress the default animation.
+            Frame.Navigate(typeof(ApplicationDetail), _storeditem, new SuppressNavigationTransitionInfo());
+        }
+
+
+        private async void collection_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_storeditem != null)
+            {
+                // If the connected item appears outside the viewport, scroll it into view.
+                collection.ScrollIntoView(_storeditem, ScrollIntoViewAlignment.Default);
+                collection.UpdateLayout();
+
+                // Play the second connected animation.
+                ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("BackConnectedAnimation");
+                if (animation != null)
+                {
+                    // Setup the "back" configuration if the API is present.
+                    if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 7))
+                    {
+                        animation.Configuration = new DirectConnectedAnimationConfiguration();
+                    }
+
+                    await collection.TryStartConnectedAnimationAsync(animation, _storeditem, "mainContent");
+                }
+
+                // Set focus on the list
+                collection.Focus(FocusState.Programmatic);
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
