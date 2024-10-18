b3e5ae8d0f5bb73f2f2c2609ca3175c1d484b2c9
limopika <c1104121@planet.kanazawa-it.ac.jp>
2024-10-18 16:18:07 +0900

    たぶん完成

--- dsGallery/dsGallery/view/IllustCollection.cs
+++ dsGallery/dsGallery/view/IllustCollection.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Windows.Data.Json;
 using Windows.Storage;
 using Windows.Storage.Search;
 
@@ -34,10 +36,53 @@ namespace dsGallery.view
             if (mill is not null)
             {
                 IReadOnlyList<StorageFile> m_illusts = await mill.GetFilesAsync(CommonFileQuery.DefaultQuery);
+                var dict = new Dictionary<string, string[]>();
+
+                try
+                {
+                    JsonValue data;
+                    bool result = JsonValue.TryParse(
+                    await FileIO.ReadTextAsync(
+                            await mill.GetFileAsync("iview.json")
+                        ), out data
+                    );
+
+                    if (result)
+                    {
+                        JsonArray _contents = data.GetObject().GetNamedArray("contents");
+
+                        foreach (JsonValue item in _contents)
+                        {
+                            dict.Add(
+                                mill.Path + item.GetObject().GetNamedString("path").Replace("/", "\\"),
+                                [
+                                  item.GetObject().GetNamedString("author"),
+                                  item.GetObject().GetNamedString("title"),
+                                  item.GetObject().GetNamedString("description")
+                                ]
+                            );
+                        }
+                    }
+                }
+                catch (Exception) { }
+
 
                 foreach (StorageFile file in m_illusts)
                 {
-                    Add(new IllustCollection(file.DisplayName, "(詳細情報なし)", file.Path));
+                    if (file.Name == "iview.json" ) continue;
+
+                    if (dict.ContainsKey(file.Path))
+                    {
+                        Add(new IllustCollection(
+                            dict[file.Path][1],
+                            dict[file.Path][2] + "\n制作者: " + dict[file.Path][0],
+                            file.Path
+                            ));
+                    }
+                    else
+                    {
+                        Add(new IllustCollection(file.DisplayName, "", file.Path));
+                    }
                 }
             }
         }
--- dsGallery/dsGallery/view/MusicCollection.cs
+++ dsGallery/dsGallery/view/MusicCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Windows.Data.Json;
 using Windows.Storage;
 using Windows.Storage.Search;
 
@@ -36,10 +37,54 @@ namespace dsGallery.view
             if (mmus is not null)
             {
                 IReadOnlyList<StorageFile> m_musics = await mmus.GetFilesAsync(CommonFileQuery.DefaultQuery);
+                var dict = new Dictionary<string, string[]>();
+
+                try
+                {
+                    JsonValue data;
+                    bool result = JsonValue.TryParse(
+                    await FileIO.ReadTextAsync(
+                            await mmus.GetFileAsync("mview.json")
+                        ), out data
+                    );
+
+                    if (result)
+                    {
+                        JsonArray _contents = data.GetObject().GetNamedArray("contents");
+
+                        foreach (JsonValue item in _contents)
+                        {
+                            dict.Add(
+                                mmus.Path + item.GetObject().GetNamedString("path").Replace("/", "\\"),
+                                [
+                                  item.GetObject().GetNamedString("author"),
+                                  item.GetObject().GetNamedString("title"),
+                                  item.GetObject().GetNamedString("jacket"),
+                                  item.GetObject().GetNamedString("description")
+                                ]
+                            );
+                        }
+                    }
+                }
+                catch (Exception) { }
 
                 foreach (StorageFile file in m_musics)
                 {
-                    Add(new MusicCollection(file.DisplayName, "(詳細情報なし)", "/Resources/nodisc.png", file));
+                    if (file.Name == "mview.json") continue;
+
+                    if (dict.ContainsKey(file.Path))
+                    {
+                        Add(new MusicCollection(
+                            dict[file.Path][1],
+                            dict[file.Path][3] + "\n制作者: " + dict[file.Path][0],
+                            dict[file.Path][2].Equals("") ? "/Resources/nodisc.png" : mmus.Path + dict[file.Path][2],
+                            file
+                            ));
+                    }
+                    else
+                    {
+                        Add(new MusicCollection(file.DisplayName, "", "/Resources/nodisc.png", file));
+                    }
                 }
             }
         }
